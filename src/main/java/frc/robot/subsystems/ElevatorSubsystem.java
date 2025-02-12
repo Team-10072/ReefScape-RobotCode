684cd779ec44eafa6e32e42f893397c092b7ed70
EliWaffles6 <elihauser4264@wilsonstudent.net>
2025-02-11 19:17:30 -0500

    Made some minor changes and deleted the ElevatorSubsystem file, replacing it with a newly renamed Elevator.java.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,8 +1,31 @@
 package frc.robot.subsystems;
 
-/*import thyme!*/
-
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem {
-    
+    private SparkMax theMotor = new SparkMax(Constants.DriveConstants.kElevatorMotorCanId, MotorType.kBrushless);
+    private AbsoluteEncoder absoluteEncoder = theMotor.getAbsoluteEncoder();
+    private RelativeEncoder relativeEncoder = theMotor.getEncoder();
+
+    public void moveTheMotor(double speed) {
+        theMotor.set(speed);
+    }
+    public void checkElevatorMotor() {
+        double position = theMotor.getAbsoluteEncoder().getPosition();
+        System.out.println(position);
+        if (position >= 0 || position <= 0) {
+            theMotor.set(0);
+        }
+    }
+    // NEXT METHOD IS UNFNISHED - do not use until fixed 
+    public void zeroElevator(boolean areYouSure, boolean areYouReallySure) {
+        if (areYouSure && areYouReallySure) {
+
+        }
+    }
 }

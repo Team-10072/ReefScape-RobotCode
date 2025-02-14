153f436821a9a1873e3b3b79d16d33850ea1f792
EliWaffles6 <elihauser4264@wilsonstudent.net>
2025-02-13 19:16:42 -0500

    I added more to the ElevatorSubsystem and got started on the endstop for the elevator.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,31 +1,55 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
+// import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.util.sendable.SendableRegistry;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ElevatorSubsystem {
-    private SparkMax theMotor = new SparkMax(Constants.DriveConstants.kElevatorMotorCanId, MotorType.kBrushless);
+/**
+ * This is the subsystem designed to operate the Elevator.
+ * It is instanted by calling new ElevatorSubsystem();
+ * 
+ */
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private final SparkMax theMotor = new SparkMax(Constants.DriveConstants.kElevatorMotorCanId, MotorType.kBrushless);
     private AbsoluteEncoder absoluteEncoder = theMotor.getAbsoluteEncoder();
-    private RelativeEncoder relativeEncoder = theMotor.getEncoder();
+    // private RelativeEncoder relativeEncoder = theMotor.getEncoder();
+
+    private double zerodPos = 0.0;
+
+    private boolean endstopTriggered = false;
 
     public void moveTheMotor(double speed) {
         theMotor.set(speed);
     }
     public void checkElevatorMotor() {
-        double position = theMotor.getAbsoluteEncoder().getPosition();
-        System.out.println(position);
+        double position = getElevatorPosition();
+        // SendableRegistry.add(new Sendable(), "Elevator Motor");
+        checkEndStop();
         if (position >= 0 || position <= 0) {
             theMotor.set(0);
         }
     }
-    // NEXT METHOD IS UNFNISHED - do not use until fixed 
+    public double getElevatorPosition() {
+        double position = absoluteEncoder.getPosition();
+        position += zerodPos;
+        return position;
+    }
     public void zeroElevator(boolean areYouSure, boolean areYouReallySure) {
         if (areYouSure && areYouReallySure) {
-
+            zerodPos = absoluteEncoder.getPosition();
+        }
+    }
+    public void checkEndStop() {
+        if (!endstopTriggered) {
+            theMotor.set(0);
+            // theMotor.
+        } else {
+            // Kalm.
         }
     }
 }

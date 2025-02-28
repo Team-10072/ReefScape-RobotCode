97531998178c9b0fb453b5bfcaeb4cb8aa6a0e97
EliWaffles6 <elihauser4264@wilsonstudent.net>
2025-02-27 19:09:51 -0500

    I created boilerplate code for a VisionTesting file, along with the imports nessessary to start.

---
+++ src/main/java/frc/robot/VisionTesting.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.vision.VisionPipeline;
+import edu.wpi.first.vision.VisionRunner;
+import edu.wpi.first.vision.VisionThread;
+import org.opencv.*;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cameraserver.CameraServerShared;
+import edu.wpi.first.cameraserver.CameraServerSharedStore;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+
+public class VisionTesting {
+
+}

5653f213e0981f7eb023e5174f2adefb78b7ca7b
EliWaffles6 <elihauser4264@wilsonstudent.net>
2025-03-04 18:44:32 -0500

    I commented out unused imports in RadioSubsystem and RobotContainer.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,8 +11,8 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
+// import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.wpilibj.PS4Controller.Button;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -20,7 +20,7 @@ import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import java.util.List;
 
 /*
--- src/main/java/frc/robot/subsystems/RadioSubsystem.java
+++ src/main/java/frc/robot/subsystems/RadioSubsystem.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
-import frc.robot.Constants;
-import java.util.*;
+// import frc.robot.Constants;
+// import java.util.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RadioSubsystem extends SubsystemBase {

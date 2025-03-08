7fd3d8321e33643c4d77a50a608eb5346c943543
EliWaffles6 <elihauser4264@wilsonstudent.net>
2025-03-08 14:02:13 -0500

    For some reason, this was required.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -80,7 +80,7 @@ public final class Constants {
   }
 
   public static final class OIConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int kDriverControllerPort = 2;
     public static final int ksecondControllerport = 1;
     
     public static final double kDriveDeadband = 0.05;
